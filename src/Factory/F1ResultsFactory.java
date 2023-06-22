31f88f1621a0c2e9d283792ca44d860e83d17cc0
Ryan Haniff <haniffryan.hr@gmail.com>
2023-06-21 21:18:36 -0400

    Javadocs for F1ResultsFactory created

--- src/Factory/F1ResultsFactory.java
+++ src/Factory/F1ResultsFactory.java
@@ -2,8 +2,20 @@ package Factory;
 
 import RequestData.*;
 
+/**
+ * The Factory where all objects of concrete classes are created and returned to
+ * main()
+ */
 public class F1ResultsFactory {
 
+    /**
+     * Function gets called in main after creating a {@link #F1ResultsFactory()}
+     * object.
+     * 
+     * @param channel is the result you want to query. Can be a year, a season, or
+     *                the latest race/qualifying results
+     * @return the concrete class object
+     */
     public Request createConnection(String channel) {
 
         if (channel.isEmpty()) {
